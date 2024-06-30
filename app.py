dddff7057da6be9a6205b91f54224f2f96e6e8b7
bitbotcoder <5197646+bitbotcoder@users.noreply.github.com>
2024-06-30 14:15:01 +0530

    fixed form

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.header("👨‍⚕️ Nature's Rx Ayur ChatBot ",divider="rainbow")
 st.write("🍀Your Ayurveda and Naturopathy Companion for Natural Healing 🍀🌿💊")
 st.info("""This app does not store or share API keys externally. Configure Your OpenAI API Key and Hit Enter""", icon="ℹ️")
 api_key = st.text_input("OpenAI Key", label_visibility="collapsed",placeholder='OpenAI API Key',type="password")
-
+key_set=False
 #api_key = open("openai_api_key.txt", "r").read().strip()
 # Instantiate LLM model
 if api_key:
@@ -20,48 +20,49 @@ if api_key:
     msgs = StreamlitChatMessageHistory(key="langchain_messages")
     agent = get_agent(GPT_Model2)
     qa = get_QA(GPT_Model2)
-
-    # Streamlit form components, using sessions state to maintain the chat history, chat_message and text components.
-    
-    with st.form("my_form"):
-        col3, col4 = st.columns([1, 1])
-        prompt = col3.text_input("Enter text:", "How to cure Common Flu?")
-        city = col4.text_input(label="Your City", value="Amritsar")
-        submitted = st.form_submit_button("Submit")
-
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = [
-            {"role": "assistant", "content": "How can I help you?"}]
-
-    for msg in st.session_state.messages:
-        st.chat_message(msg["role"]).write(msg["content"])
-
-    st.chat_message("user").write(prompt)
-    
-    if submitted:
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        response, doctors = get_response(qa, agent, prompt, city)
-        if response:
-            st.chat_message("assistant").write(response)
-            st.chat_message("assistant").write(
-                f"List of Ayurveda Practioners in {city} and their addresses :")
-            st.chat_message("assistant").write(doctors)
-            st.session_state["messages"].append(
-                {"role": "assistant", "content": response})
-            st.session_state["messages"].append(
-                {"role": "assistant", "content": f"List of Ayurveda Practioners in {city} and their addresses :"})
-            st.session_state["messages"].append(
-                {"role": "assistant", "content": doctors})
+    key_set=True
 else:
     st.warning("OpenAI API Key is not configured. Enter your OpenAI Key to access GPT models.", icon="❎")
+
+# Streamlit form components, using sessions state to maintain the chat history, chat_message and text components.
+
+with st.form("my_form"):
+    c3, c4 = st.columns([1, 1])
+    prompt = c3.text_input("Enter text:", value="How to cure Common Flu?")
+    city = c4.text_input(label="Your City", value="Amritsar")
+    submitted = st.form_submit_button("Submit")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+st.chat_message("user").write(prompt)
+
+if submitted and key_set:
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    response, doctors = get_response(qa, agent, prompt, city)
+    if response:
+        st.chat_message("assistant").write(response)
+        st.chat_message("assistant").write(
+            f"List of Ayurveda Practioners in {city} and their addresses :")
+        st.chat_message("assistant").write(doctors)
+        st.session_state["messages"].append(
+            {"role": "assistant", "content": response})
+        st.session_state["messages"].append(
+            {"role": "assistant", "content": f"List of Ayurveda Practioners in {city} and their addresses :"})
+        st.session_state["messages"].append(
+            {"role": "assistant", "content": doctors})
 
 
 st.divider()    
 st.write("📢 Share with wider community")
 social_media_links = [
-        "https://x.com/intent/tweet?hashtags=streamlit%2Cpython&text=Check%20out%20this%20awesome%20Streamlit%20app%20I%20built%0A&url=https%3A%2F%2Fautoml-wiz.streamlit.app",
-        "https://www.linkedin.com/sharing/share-offsite/?summary=https%3A%2F%2Fautoml-wiz.streamlit.app%20%23streamlit%20%23python&title=Check%20out%20this%20awesome%20Streamlit%20app%20I%20built%0A&url=https%3A%2F%2Fautoml-wiz.streamlit.app",
-        "https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&u=https%3A%2F%2Fautoml-wiz.streamlit.app",
+        "https://x.com/intent/tweet?hashtags=streamlit%2Cpython&text=Check%20out%20this%20awesome%20Streamlit%20app%20I%20built%0A&url=https%3A%2F%2Fayurbot9.streamlit.app",
+        "https://www.linkedin.com/sharing/share-offsite/?summary=https%3A%2F%2Fayurbot9.streamlit.app%20%23streamlit%20%23python&title=Check%20out%20this%20awesome%20Streamlit%20app%20I%20built%0A&url=https%3A%2F%2Fayurbot9.streamlit.app",
+        "https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&u=https%3A%2F%2Fayurbot9.streamlit.app",
         "https://github.com/bitbotcoder/ayurbot"
     ]
 social_media_icons = SocialMediaIcons(social_media_links, colors=["white"] * len(social_media_links))
